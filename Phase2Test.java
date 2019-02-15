4bf468af5fa118ddbf6204e9842b70b94499041a
F. Payton Guthrie <paytguth@gmail.com>
2019-02-15 12:24:17 -0800

    All hints written. Ready for revision
    -Removed first/second person
    -Repeated hints in a test are strings
    -Removed some assumptions but I did keep a few more in this one because I felt they were reasonable. Please feel free to remove/edit them if you feel they will be misleading

--- Phase2Test.java
+++ Phase2Test.java
@@ -51,7 +51,6 @@ public class Phase2Test {
     /* check that the hash table contains the appropriate mappings */
     private static <K,V> void check(K[] keySet, V[] valSet, HashTable<K,V> hm) {
         assert keySet.length == valSet.length;
-
         assertEquals("HashTable invariant not satisfied: Size value of map and number of keys were not equal", keySet.length, hm.getSize());
         for (int i = 0; i < keySet.length; i++) {
             assertEquals("Value was not able to be retrieved from map", valSet[i], hm.get(keySet[i]));
@@ -81,15 +80,15 @@ public class Phase2Test {
     public void test210PutGet() {
         HashTable<Integer,Integer> hm = new HashTable<Integer,Integer>();
         for (int i = 0; i < 8; i++) {;
-            assertEquals("Your .put function did not return null when there were no duplicate keys in map", null, hm.put(i,i));
-            assertEquals("Your .put function needs to increase the size of the map by 1 when a new pair is added.", i+1, hm.getSize());
-            assertEquals("You increased the capacity of the buckets array when it didn't need it. This test doesn't require any rehashing/load factor business so don't worry about that yet.", 17, hm.getCapacity());
+            assertEquals("The .put function did not return null when there were no duplicate keys in map", null, hm.put(i,i));
+            assertEquals("The .put function needs to increase the size of the map by 1 when a new pair is added.", i+1, hm.getSize());
+            assertEquals("The capacity of the buckets array increased when it didn't need it. This test doesn't require any rehashing/load factor business yet", 17, hm.getCapacity());
         }
         for (int i = 0; i < 8; i++) {
-            assertEquals("Your .get() function didn't return the correct value. In this test, every value should be the first Pair in your array of linked lists which will be indexed by key", new Integer(i), hm.get(i));
+            assertEquals("The .get() function didn't return the correct value. In this test, every value should be the first Pair in an array of linked lists which will be indexed by key", new Integer(i), hm.get(i));
         }
         for (int i = 8; i < 17; i++) {
-            assertEquals("Your .get function needs to return null when it is called with a key that doesn't exist in the map.", null, hm.get(i));
+            assertEquals("The .get function needs to return null when it is called with a key that doesn't exist in the map.", null, hm.get(i));
         }
     }
 
@@ -99,7 +98,7 @@ public class Phase2Test {
         Integer[] keys = new Integer[]{0,1,2,3,22,23,24,25};
         Integer[] vals = new Integer[]{0,1,2,3,22,23,24,25};
         HashTable<Integer,Integer> hm = make(keys, vals);
-        check("\nFor this test you need to hash keys to know which index to store it in. For example if the key is 22 and the capacity is 17, it would be stored in index 5 because 22 % 17 is 5", keys, vals, hm);
+        check("\nFhis test requires hash keys provided by a hash function to know which index to store it in. For example if the key is 22 and the capacity is 17, it would be stored in index 5 because 22 % 17 is 5", keys, vals, hm);
     }
 
     @Test
@@ -110,8 +109,8 @@ public class Phase2Test {
         HashTable<Integer,Integer> hm = make(keys, vals);
 
         for (int i = 0; i < 10; i++) {
-            assertEquals("Your .put function needs to return the previous value of a given key when .put is called with a key that was already in the map. Don't forget to overrite the pre-xisting Pair's old value with the new one.", new Integer(i), hm.put(i,i+1));
-            assertEquals("The size of your map wasn't correct. .put should increase the size when a new key is added but it shouldn't change the size when a pre-existing key is overwritten", 10, hm.getSize());
+            assertEquals("The .put function needs to return the previous value of a given key when .put is called with a key that was already in the map. Don't forget to overrite the pre-xisting Pair's old value with the new one.", new Integer(i), hm.put(i,i+1));
+            assertEquals("The size of the map wasn't correct. .put should increase the size when a new key is added but it shouldn't change the size when a pre-existing key is overwritten", 10, hm.getSize());
         }
 
 
@@ -123,7 +122,7 @@ public class Phase2Test {
         Integer[] keys = new Integer[]{0,1,2,3,17,18,19};
         Integer[] vals = new Integer[]{0,1,2,3,17,18,19};
         HashTable<Integer,Integer> hm = make(keys, vals);
-        check("\nThis test is testing your put and get functions with collisions.\nRemember that if a hash value collides with another, the new Pair should be appended to the end of the linked list.\nTake a look at the Pair class and notice its similarites to a linked list", keys, vals, hm);
+        check("\nThis test is testing the put and get functions with collisions.\nRemember that if a hash value collides with another, the new Pair should be appended to the end of the linked list.\nTake a look at the Pair class and notice its similarites to a linked list", keys, vals, hm);
     }
 
     @Test
@@ -135,8 +134,8 @@ public class Phase2Test {
         HashTable<Integer,Integer> hm = make(keys, vals);
 
         for (int i = 0; i < keys.length; i++) {
-          assertEquals("Your put function did not properly return the previous value stored with that key", vals[i], hm.put(keys[i],keys[i]+1));
-          assertEquals("the size of your map wasn't correct. .put should not change the size if it is replacing a duplicate key.", 7, hm.getSize());
+          assertEquals("The put function did not properly return the previous value stored with that key", vals[i], hm.put(keys[i],keys[i]+1));
+          assertEquals("The size of the map wasn't correct. .put should not change the size if it is replacing a duplicate key.", 7, hm.getSize());
         }
 
 
@@ -156,10 +155,10 @@ public class Phase2Test {
         int ki = 0;
         while (i < 35) {
             if (ki < keys.length && keys[ki] == i) {
-                assertEquals("Your containsKey() didn't return true when called with a key that exists in the map.\nRemember that .get already looks for a returns a non-null value when called with a key in the map", true, hm.containsKey(i));
+                assertEquals("The containsKey() didn't return true when called with a key that exists in the map.\nRemember that .get already looks for a returns a non-null value when called with a key in the map", true, hm.containsKey(i));
                 ki++;
             } else {
-                assertEquals("Your containsKey() didn't return false when called with a key that doesn't exist in the map.\nRemember .get should return null when a key is not in the map", false, hm.containsKey(i));
+                assertEquals("Thw containsKey() didn't return false when called with a key that doesn't exist in the map.\nRemember .get should return null when a key is not in the map", false, hm.containsKey(i));
             }
             i++;
         }
@@ -176,10 +175,10 @@ public class Phase2Test {
         int ki = 0;
         while (i < 34) {
             if (ki < keys.length && keys[ki] == i) {
-                assertEquals("Your containsKey returned false when called with a valid key in a map with collisions.", true, hm.containsKey(i));
+                assertEquals("The containsKey function returned false when called with a valid key in a map with collisions.", true, hm.containsKey(i));
                 ki++;
             } else {
-                assertEquals("Your containsKey returned true when called with a nonexistant key in a map with collisions.", false, hm.containsKey(i));
+                assertEquals("The containsKey function returned true when called with a nonexistant key in a map with collisions.", false, hm.containsKey(i));
             }
             i++;
         }
@@ -193,8 +192,8 @@ public class Phase2Test {
         HashTable<Integer,Integer> hm = make(keys, vals);
 
         for (int i = 0; i < 10; i++) {
-            assertEquals("Your remove function didn't properly return the value of the removed key. This test doesn't have any collisons", vals[i], hm.remove(keys[i]));
-            assertEquals("The size of your hashmap wasn't correct after removal. Remove should always decrease the size if the key existed in the map", 10-i-1, hm.getSize());
+            assertEquals("Thw remove function didn't properly return the value of the removed key. This test doesn't have any collisons", vals[i], hm.remove(keys[i]));
+            assertEquals("The size of the hashmap wasn't correct after removal. Remove should always decrease the size if the key existed in the map", 10-i-1, hm.getSize());
         }
 
     }
@@ -206,19 +205,21 @@ public class Phase2Test {
         Integer[] keys = new Integer[]{0,1,2,17,18,19};
         Integer[] vals = new Integer[]{0,1,2,17,18,19};
         HashTable<Integer,Integer> hm = make(keys, vals);
+        String removeSizeError = ".remove didn't properly modify the size";
         assertEquals("When removing the first pair in an index, the next pair should be the head of the list. This test failed because it did not return the correct value of the removed Pair.", vals[0], hm.remove(keys[0]));
-        assertEquals(".remove didn't properly modify the size", 5, hm.getSize());
-        assertEquals("When removing the last pair in a list, the pair before it should point to null. This test failed because it did not return the correct value of the removed Pair.", vals[5], hm.remove(keys[5]));
-        assertEquals(".remove didn't properly modify the size", 4, hm.getSize());
-        assertEquals("When removing the last pair in a list, the pair before it should point to null. This test failed because it did not return the correct value of the removed Pair.", vals[4], hm.remove(keys[4]));
-        assertEquals(".remove didn't properly modify the size", 3, hm.getSize());
-        assertEquals("When removing the only Pair at an index, this index should contain nothing. This test failed because it did not return the correct value of the removed Pair.", vals[2], hm.remove(keys[2]));
-        assertEquals(".remove didn't properly modify the size", 2, hm.getSize());
-        assertEquals("When removing the only Pair at an index, this index should contain nothing. This test failed because it did not return the correct value of the removed Pair.", vals[1], hm.remove(keys[1]));
-        assertEquals(".remove didn't properly modify the size", 1, hm.getSize());
-        assertEquals("When removing the only Pair at an index, this index should contain nothing. This test failed because it did not return the correct value of the removed Pair.", vals[3], hm.remove(keys[3]));
-        assertEquals(".remove didn't properly modify the size", 0, hm.getSize());
-
+        assertEquals(removeSizeError, 5, hm.getSize());
+        String lastPairHint = "When removing the last pair in a list, the pair before it should point to null. This test failed because it did not return the correct value of the removed Pair.";
+        assertEquals(lastPairHint, vals[5], hm.remove(keys[5]));
+        assertEquals(removeSizeError, 4, hm.getSize());
+        assertEquals(lastPairHint, vals[4], hm.remove(keys[4]));
+        assertEquals(removeSizeError, 3, hm.getSize());
+        String removingSingleNodeHint = "When removing the only Pair at an index, this index should contain nothing. This test failed because it did not return the correct value of the removed Pair.";
+        assertEquals(removingSingleNodeHint, vals[2], hm.remove(keys[2]));
+        assertEquals(removeSizeError, 2, hm.getSize());
+        assertEquals(removingSingleNodeHint, vals[1], hm.remove(keys[1]));
+        assertEquals(removeSizeError, 1, hm.getSize());
+        assertEquals(removingSingleNodeHint, vals[3], hm.remove(keys[3]));
+        assertEquals(removeSizeError, 0, hm.getSize());
     }
 
 
@@ -233,11 +234,11 @@ public class Phase2Test {
 
         for (int i = 0; i < 13; i++) {
             hm.put(keys[i], vals[i]);
-            assertEquals("The load factor does not exceed 0.8, your hashtable's capacity changed when it shouldn't have. Make sure you're checking the load factor correctly", 17, hm.getCapacity());
+            assertEquals("The load factor does not exceed 0.8. The hashtable's capacity changed when it shouldn't have. Make sure you're checking the load factor correctly", 17, hm.getCapacity());
         }
 
         hm.put(keys[13], vals[13]);
-        assertEquals("The hashtable's load factor just exceeded 0.8, and should have doubled capacity from the default 17 to 34. Make sure you're checking the load factor correctly", 34, hm.getCapacity());
+        assertEquals("The hashtable's load factor just exceeded 0.8, and should have doubled capacity from the default 17 to 34.", 34, hm.getCapacity());
     }
 
     @Test
@@ -250,8 +251,8 @@ public class Phase2Test {
         for (int i = 0; i < keys.length; i++) {
             hm.put(keys[i], vals[i]);
         }
-        assertEquals("The hashtable's load factor just exceeded 0.8, and should have doubled capacity from the default 17 to 34. Make sure you're checking the load factor correctly", 34, hm.getCapacity());
-        check("\n Hashtable didn't rehash correctly. Create a new hashtable with double the size and put all the old hashtable values into the new one with .put", keys, vals, hm);
+        assertEquals("The hashtable's load factor just exceeded 0.8, and should have doubled capacity from the default 17 to 34.", 34, hm.getCapacity());
+        check("\nHashtable didn't rehash correctly. To start out, create a new hashtable with double the size and put all the old hashtable values into the new one with .put", keys, vals, hm);
     }
 
 
@@ -262,7 +263,7 @@ public class Phase2Test {
         Integer[] vals = new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12};
 
         HashTable<String,Integer> hm = make(keys, vals);
-        check("This test is using string values as keys. Look up the .hashCode() method and use it in your hash formula", keys, vals, hm);
+        check("\nThis test is using string values as keys. Look up the .hashCode() method and use it to calculate indices in the hash formula", keys, vals, hm);
 
     }
 
@@ -307,12 +308,12 @@ public class Phase2Test {
             }
         }
         // check that the mappings are equivalent:
-        assertEquals(truth.size(), hm.getSize());
+        assertEquals("In a huge test, the size of the hashmap was incorrect.", truth.size(), hm.getSize());
         for (String k : truth.keySet()) {
-            assertTrue(hm.containsKey(k));
-            assertEquals(truth.get(k), hm.get(k));
+            assertTrue("The hashmap did not contain a key that it should have", hm.containsKey(k));
+            assertEquals("The value at a key stored in the hashmap was not correct", truth.get(k), hm.get(k));
         }
-        assertEquals(hm.getCapacity(), 2176);
+        assertEquals("The capacity of the hashmap was incorrect", hm.getCapacity(), 2176);
     }
 
     //@Test
